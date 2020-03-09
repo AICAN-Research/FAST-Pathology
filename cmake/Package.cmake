5595c7b452b2f8bd568693e69fb96267597f3ece
Erik Smistad <ersmistad@gmail.com>
2020-03-09 15:56:08 +0100

    Set up working cmake install script

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -1,19 +1,69 @@
 
 # Install pathology application
-install(TARGETS fastPathology
+install(
+    TARGETS fastPathology
     DESTINATION bin
+)
+# License file
+install(
+    FILES LICENSE.md
+    DESTINATION licenses/fastPathology
 )
 
 # Install FAST dependency
-install(
-    DIRECTORY ${FAST_BINARY_DIR}
-    DESTINATION bin
-)
+if(WIN32)
+    install(
+        DIRECTORY ${FAST_BINARY_DIR}
+        DESTINATION bin
+        FILES_MATCHING PATTERN "*.dll"
+    )
+else()
+    install(
+        DIRECTORY ${FAST_BINARY_DIR}
+        DESTINATION bin
+        FILES_MATCHING PATTERN "*.so"
+    )
+endif()
 install(
     DIRECTORY ${FAST_BINARY_DIR}/../kernels/
-    DESTINATION bin
+    DESTINATION kernels
+)
+install(
+    DIRECTORY ${FAST_BINARY_DIR}/../plugins/
+    DESTINATION plugins
+)
+install(
+    DIRECTORY ${FAST_BINARY_DIR}/../doc/
+    DESTINATION doc
+)
+install(
+    DIRECTORY ${FAST_BINARY_DIR}/../licenses/
+    DESTINATION licenses
+)
+install(
+    DIRECTORY
+    DESTINATION kernel_binaries
+    DIRECTORY_PERMISSIONS OWNER_READ OWNER_EXECUTE OWNER_WRITE GROUP_READ GROUP_EXECUTE GROUP_WRITE WORLD_READ WORLD_WRITE WORLD_EXECUTE
 )
 
+# Setup fast_configuration.txt file
+set(FILE_CONTENT "TestDataPath = @ROOT@/data/
+KernelSourcePath = @ROOT@/kernels/
+KernelBinaryPath = @ROOT@/kernel_binaries/
+DocumentationPath = @ROOT@/doc/
+PipelinePath = @ROOT@/pipelines/
+LibraryPath = @ROOT@/bin/
+QtPluginsPath = @ROOT@/plugins/")
+
+# Write file
+file(WRITE ${PROJECT_BINARY_DIR}/fast_configuration_install.txt ${FILE_CONTENT})
+
+# Install file
+install(
+    FILES ${PROJECT_BINARY_DIR}/fast_configuration_install.txt
+    DESTINATION bin
+    RENAME fast_configuration.txt
+)
 
 if(WIN32 AND NOT UNIX)
     ## Windows
