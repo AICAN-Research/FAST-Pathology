73d90ed7525c93cf4e76362f0dcd42afc1befa24
Erik Smistad <ersmistad@gmail.com>
2022-06-07 13:36:35 +0200

    New attempt at macOS runInThread fix

--- source/main.cpp
+++ source/main.cpp
@@ -4,11 +4,6 @@
 using namespace fast;
 
 int main(int argc, char** argv) {
-#if defined(__APPLE__) || defined(__MACOSX)
-    // Mac hack for runInThread https://github.com/AICAN-Research/FAST-Pathology/issues/26
-    std::cout << "DISABLE AFFINITY GL CHECK" << std::endl;
-    QCoreApplication::setAttribute(Qt::AA_DontCheckOpenGLContextThreadAffinity);
-#endif
     CommandLineParser parser("FastPathology", "An open-source platform for deep learning-based research and decision support in digital pathology");
     parser.parse(argc, argv);
 
