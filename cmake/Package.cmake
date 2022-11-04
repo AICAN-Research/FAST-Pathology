4e0b3b794d6649855c47fd1bed5f34bf15f1f014
André Pedersen <andrped94@gmail.com>
2022-11-04 23:17:22 +0100

    did not work to revert .// - falling back

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -15,14 +15,14 @@ endif()
 if(APPLE)
 	install(
 		DIRECTORY
-		DESTINATION bin)
+		DESTINATION ../bin)
 endif()
 
 # License file
 if(APPLE)
 	install(
 		FILES LICENSE.md
-		DESTINATION licenses/fastpathology
+		DESTINATION ../licenses/fastpathology
 	)
 else()
 	install(
@@ -38,10 +38,10 @@ if(WIN32)
 			FILES_MATCHING PATTERN "*.dll")
 elseif(APPLE)
 	install(DIRECTORY ${FAST_BINARY_DIR}/../lib/
-			DESTINATION lib
+			DESTINATION ../lib
 			FILES_MATCHING PATTERN "*.dylib*")
 	install(DIRECTORY ${FAST_BINARY_DIR}/../lib/
-			DESTINATION lib
+			DESTINATION ../lib
 			FILES_MATCHING PATTERN "*.so*")
 else()
 	install(DIRECTORY ${FAST_BINARY_DIR}/../lib/
@@ -66,36 +66,69 @@ else()
 			)
 endif()
 
-install(
-	DIRECTORY ${FAST_BINARY_DIR}/../kernels/
-	DESTINATION kernels
-)
-install(
-	DIRECTORY ${FAST_BINARY_DIR}/../plugins/
-	DESTINATION plugins
-)
-install(
-	FILES ${FAST_BINARY_DIR}/../doc/images/fast_icon.ico ${FAST_BINARY_DIR}/../doc/images/fast_icon.png
-	DESTINATION doc/images
-)
-install(
-	DIRECTORY ${FAST_BINARY_DIR}/../doc/fonts/
-	DESTINATION doc/fonts
-)
-install(
-	DIRECTORY ${FAST_BINARY_DIR}/../licenses/
-	DESTINATION licenses
-)
+if(APPLE)
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../kernels/
+		DESTINATION ../kernels
+	)
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../plugins/
+		DESTINATION ../plugins
+	)
+	install(
+		FILES ${FAST_BINARY_DIR}/../doc/images/fast_icon.ico ${FAST_BINARY_DIR}/../doc/images/fast_icon.png
+		DESTINATION ../doc/images
+	)
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../doc/fonts/
+		DESTINATION ../doc/fonts
+	)
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../licenses/
+		DESTINATION ../licenses
+	)
 
-# add Data folder for storing saved models, icons, pipelines and other stuff, and move necessary folders
-install(
-	DIRECTORY ${PROJECT_BINARY_DIR}/../data/Icons
-	DESTINATION data
-)
-install(
-	DIRECTORY ${PROJECT_BINARY_DIR}/../data/pipelines
-	DESTINATION data
-)
+	# add Data folder for storing saved models, icons, pipelines and other stuff, and move necessary folders
+	install(
+		DIRECTORY ${PROJECT_BINARY_DIR}/../data/Icons
+		DESTINATION ../data
+	)
+	install(
+		DIRECTORY ${PROJECT_BINARY_DIR}/../data/pipelines
+		DESTINATION ../data
+	)
+else()
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../kernels/
+		DESTINATION kernels
+	)
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../plugins/
+		DESTINATION plugins
+	)
+	install(
+		FILES ${FAST_BINARY_DIR}/../doc/images/fast_icon.ico ${FAST_BINARY_DIR}/../doc/images/fast_icon.png
+		DESTINATION doc/images
+	)
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../doc/fonts/
+		DESTINATION doc/fonts
+	)
+	install(
+		DIRECTORY ${FAST_BINARY_DIR}/../licenses/
+		DESTINATION licenses
+	)
+
+	# add Data folder for storing saved models, icons, pipelines and other stuff, and move necessary folders
+	install(
+		DIRECTORY ${PROJECT_BINARY_DIR}/../data/Icons
+		DESTINATION data
+	)
+	install(
+		DIRECTORY ${PROJECT_BINARY_DIR}/../data/pipelines
+		DESTINATION data
+	)
+endif()
 
 # Setup fast_configuration.txt file
 if(WIN32)
@@ -116,7 +149,7 @@ elseif(APPLE)
 	QtPluginsPath = @ROOT@/../plugins/")
 
 else()
-	# UNIX
+	# LINUX
 	set(FILE_CONTENT "KernelSourcePath = @ROOT@/kernels/
 	DocumentationPath = @ROOT@/doc/
 	LibraryPath = @ROOT@/lib/
